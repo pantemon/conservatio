d49fb9f1a149eacccf344bfa9bf9bed9a4ac3c87
Yurka2048 <44337171+Yurka2048@users.noreply.github.com>
2023-01-19 17:46:34 +0100

    deleted all the stuff related to netlify

--- functions/main.js
+++ functions/main.js
@@ -238,18 +238,7 @@ bot.action('next', (ctx) => {
 })
 
 // launches the bot
-// bot.launch()
+bot.launch()
 
 // creates initial node-schedule jobs map from the db
 createJobsList(bot, jobsList)
-
-// AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
-exports.handler = async event => {
-    try {
-      await bot.handleUpdate(JSON.parse(event.body))
-      return { statusCode: 200, body: "" }
-    } catch (e) {
-      console.error("error in handler:", e)
-      return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" }
-    }
-  }
